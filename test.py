9c3872faac880e1db0733862abd6259e18134ebf
JaswanthT1 <jaswanth001@gmail.com>
2023-12-23 17:38:56 +0530

    pass

---
+++ test.py
@@ -0,0 +1,14 @@
+phrase = "Stewie Griffin"
+print( phrase.replace("Stewie","Peter"))
+my_num = -5
+print(abs(my_num)) 
+print(pow(4,2))
+print(max(4,2))
+print(min(4,2))
+name = input("Enter your name: ") 
+print("Hello" +name+"!")
+
+
+from math import *
+print(sqrt(36)) 
+print(round(4.6)) #ceil and also #floor for removing decimal 
